0aaea5b360b7d03ff85feef02b2f6919d19e6daf
Iris-Calvin <izzy.krak+github@gmail.com>
2025-03-09 17:50:07 +0100

    Update InfoPage.tsx
    add arrival info

--- src/pages/InfoPage.tsx
+++ src/pages/InfoPage.tsx
@@ -1,9 +1,10 @@
-import { Container, Box } from '@mantine/core'
+import { Container, Box } from '@mantine/core';
 import { useMediaQuery } from '@mantine/hooks';
 
 import SleepoverComponent from '../components/SleepoverComponent';
 import PresentsComponent from '../components/PresentsComponent';
 import ContactsComponent from '../components/ContactsComponent';
+import ArrivalInfoComponent from '../components/ArrivalInfoComponent';
 
 const CouplePage = () => {
     const isDesktop = useMediaQuery('(max-width: 1200px)');
@@ -12,6 +13,9 @@ const CouplePage = () => {
         <Container size={"sm"}>
             <Box pt={"2rem"} pb={"2rem"} >
                 <PresentsComponent />
+            </Box>
+            <Box pt={"2rem"} >
+                <ArrivalInfoComponent />
             </Box>
             <Box pt={"2rem"} >
                 <SleepoverComponent />
