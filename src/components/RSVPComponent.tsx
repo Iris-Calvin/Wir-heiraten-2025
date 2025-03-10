e12e19e72d7abd4c79876570eb8932999a1cb575
Iris-Calvin <izzy.krak+github@gmail.com>
2025-03-10 23:45:52 +0100

    Update RSVPComponent.tsx

--- src/components/RSVPComponent.tsx
+++ src/components/RSVPComponent.tsx
@@ -1,31 +1,39 @@
 import { Button, Text, Box } from '@mantine/core';
-
 import { useNavigate } from 'react-router-dom';
-
 import styles from '../css/App.module.css';
 
 const RSVPComponent = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    return (
-        <Box pt={"xs"} pb={"5rem"}>
-            <Button variant="default" onClick={() => navigate("/rsvp")}
-                style={{
-                padding: "10px 20px",
-                color: "white",
-                border: "none",
-                borderRadius: "0px",
-                cursor: "pointer",
-                zIndex: 10
-                }}
-                className={styles.btn_bg}>
-                <Text size='md'>Anmelden</Text>
-            </Button>
-            <Text style={{ textAlign: 'center' }} pt={"xs"}>
-                Rückmeldung bitte bis zum 31.03.2025.
-            </Text>
-        </Box>
-    );
+  return (
+    <Box pt={"xs"} pb={"5rem"}>
+      <Button
+        variant="default"
+        onClick={() => navigate("/rsvp")}
+        style={{
+          padding: "10px 20px",
+          color: "white",
+          border: "none",
+          borderRadius: "0px",
+          cursor: "pointer",
+          zIndex: 10,
+        }}
+        className={styles.btn_bg}
+      >
+        <Text size="md">Anmelden</Text>
+      </Button>
+      <Text
+        style={{
+          textAlign: "center",
+          position: "relative",
+          zIndex: 20, // Höherer z-index, um den Text in den Vordergrund zu rücken
+        }}
+        pt={"xs"}
+      >
+        Rückmeldung bitte bis zum 31.03.2025.
+      </Text>
+    </Box>
+  );
 };
-  
-  export default RSVPComponent;
+
+export default RSVPComponent;
