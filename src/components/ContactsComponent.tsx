b37b0b2fb4f41ccbe99ec5c77632c3481cb8bca0
Iris-Calvin <izzy.krak+github@gmail.com>
2025-03-10 21:52:14 +0100

    Update ContactsComponent.tsx

--- src/components/ContactsComponent.tsx
+++ src/components/ContactsComponent.tsx
@@ -1,25 +1,23 @@
-import { Container, Box, Stack, Title, Text } from '@mantine/core';
+import { Container, Box, Stack, Title, Text, List } from '@mantine/core';
 
 const ContactsComponent = () => {
   return (
-    <>
-        <Container size="md">
-            <Stack>
-                <Box>
-                    <Title order={4} fw={700} style={{ textAlign: 'left' }}>
-                        Du brauchst einen Ansprechpartner?
-                    </Title>
-                   <Text style={{ textAlign: 'left' }}>
-                   Bitte wende dich an die Trauzeugen:
-                  </Text>
-                  <List style={{ textAlign: 'left' }} pl={'lg'}>
-                      <List.Item>🩷Sabrina: +43 699 17043553</List.Item>
-                      <List.Item>💙Quentin: +43 699 17711313</List.Item>
-                  </List>
-                </Box>
-            </Stack>
-        </Container>
-    </>
+    <Container size="md">
+      <Stack>
+        <Box>
+          <Title order={4} fw={700} style={{ textAlign: 'left' }}>
+            Du brauchst einen Ansprechpartner?
+          </Title>
+          <Text style={{ textAlign: 'left' }}>
+            Bitte wende dich an die Trauzeugen:
+          </Text>
+          <List style={{ textAlign: 'left' }} pl={'lg'}>
+            <List.Item>🩷 Sabrina: +43 699 17043553</List.Item>
+            <List.Item>💙 Quentin: +43 699 17711313</List.Item>
+          </List>
+        </Box>
+      </Stack>
+    </Container>
   );
 };
 
