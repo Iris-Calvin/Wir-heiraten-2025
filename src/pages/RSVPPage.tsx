bcc8c5edfd435c903caa5d7bc33159a7ccaad882
Iris-Calvin <izzy.krak+github@gmail.com>
2025-03-11 12:34:02 +0100

    Update RSVPPage.tsx

--- src/pages/RSVPPage.tsx
+++ src/pages/RSVPPage.tsx
@@ -11,17 +11,29 @@ const RSVPPage = () => {
     const isMobile = useMediaQuery('(max-width: 576px)');
 
     return (
-        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeXRN_0K57mCCoY47wwOBAjXmSab--n_H9mXfQDvk8C0r6EYQ/viewform?embedded=true" 
-        width={isMobile ? "80%" : "568px"}
-        height="944"
-        style={{
+        //<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeXRN_0K57mCCoY47wwOBAjXmSab--n_H9mXfQDvk8C0r6EYQ/viewform?embedded=true" 
+        //width={isMobile ? "80%" : "568px"}
+        //height="944"
+        //style={{
+            //border: 'none',
+            //overflow: 'hidden', /* Prevent the scrollbars from appearing */
+            //width: '100%', /* Ensure the iframe fits within the container */
+            //position: 'relative', /* Optional: to better control iframe positioning */
+        //}}>
+        //Loading...
+    //</iframe>
+        <iframe
+          src="https://docs.google.com/forms/d/e/1FAIpQLSeXRN_0K57mCCoY47wwOBAjXmSab--n_H9mXfQDvk8C0r6EYQ/viewform?embedded=true"
+          width={isMobile ? "80%" : "568px"}
+          height="1200" // feste Höhe, die den gesamten Inhalt abdeckt
+          scrolling="no"
+          style={{
             border: 'none',
-            overflow: 'hidden', /* Prevent the scrollbars from appearing */
-            width: '100%', /* Ensure the iframe fits within the container */
-            position: 'relative', /* Optional: to better control iframe positioning */
-        }}>
-        Loading...
-    </iframe>
+            position: 'relative',
+          }}
+        >
+          Loading...
+        </iframe>
 
     );
 };
